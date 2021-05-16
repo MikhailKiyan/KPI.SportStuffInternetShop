72537c1156c871168ce886166210b6f8ae2497e0
Mikhail Kiyan <Kiyan@ukr.net>
2021-05-16 22:37:02 +0300

    Added Identity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,8 +6,7 @@ using KPI.SportStuffInternetShop.Domains.Identity;
 namespace KPI.SportStuffInternetShop.Data {
 
     public class ApplicationDbContext :
-            DbContext {
-            //IdentityDbContext<User, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken> {
+            IdentityDbContext<User, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken> {
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
         //public DbSet<Domains.Product> Products { get; set; }
