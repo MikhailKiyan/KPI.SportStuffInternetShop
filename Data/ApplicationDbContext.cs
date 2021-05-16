b48b5496ad6f81b86dca406e3baac921f4e67385
Mikhail Kiyan <Kiyan@ukr.net>
2021-05-16 23:54:01 +0300

    Removed all migrations

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,7 +7,8 @@ using System.Reflection;
 namespace KPI.SportStuffInternetShop.Data {
 
     public class ApplicationDbContext :
-            IdentityDbContext<User, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken> {
+            DbContext {
+            //IdentityDbContext<User, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken> {
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
@@ -15,10 +16,10 @@ namespace KPI.SportStuffInternetShop.Data {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
 
-        public DbSet<Domains.Product> Products { get; set; }
+        //public DbSet<Domains.Product> Products { get; set; }
 
-        public DbSet<Domains.ProductBrand> ProductBrands { get; set; }
+        //public DbSet<Domains.ProductBrand> ProductBrands { get; set; }
 
-        public DbSet<Domains.ProductType> ProductTypes { get; set; }
+        //public DbSet<Domains.ProductType> ProductTypes { get; set; }
     }
 }
