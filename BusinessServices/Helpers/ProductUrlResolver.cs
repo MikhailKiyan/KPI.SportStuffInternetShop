773522f285841e1190938775f44ace4c54879a8c
Mikhail Kiyan <Kiyan@ukr.net>
2021-05-18 22:40:13 +0300

    Added AutoMapper Property Resolver

---
+++ BusinessServices/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using AutoMapper;
+using Model = KPI.SportStuffInternetShop.Models.ResponseModels;
+using Domain = KPI.SportStuffInternetShop.Domains;
+
+namespace KPI.SportStuffInternetShop.BusinessServices.Helpers {
+    public class ProductUrlResolver : IValueResolver<Domain.Product, Model.Product, string> {
+        readonly IConfiguration config;
+
+        public ProductUrlResolver(IConfiguration config) {
+            this.config = config;
+        }
+
+        public string Resolve(
+                Domain.Product source,
+                Model.Product destination,
+                string destMember,
+                ResolutionContext context) {
+
+            if (!string.IsNullOrEmpty(source.PictureUrl)) return this.config["ApiUrl"] + source.PictureUrl;
+            else return null;
+        }
+    }
+}
--- BusinessServices/MappingProfiles/ProductMappingProfile.cs
+++ BusinessServices/MappingProfiles/ProductMappingProfile.cs
@@ -1,13 +1,15 @@
 ﻿using AutoMapper;
 using Model = KPI.SportStuffInternetShop.Models.ResponseModels;
 using Domain = KPI.SportStuffInternetShop.Domains;
+using KPI.SportStuffInternetShop.BusinessServices.Helpers;
 
 namespace KPI.SportStuffInternetShop.BusinessServices.MappingProfiles {
     public class ProducMappingProfiles : Profile {
         public ProducMappingProfiles() {
             this.CreateMap<Domain.Product, Model.Product>()
                 .ForMember(x => x.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-                .ForMember(x => x.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+                .ForMember(x => x.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(x => x.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
         }
     }
 }
