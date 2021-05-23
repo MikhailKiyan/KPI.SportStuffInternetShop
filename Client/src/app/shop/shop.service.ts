675d321c7c6f636f584439e17c477e34d7316daf
Mikhail Kiyan <Kiyan@ukr.net>
2021-05-23 22:39:58 +0300

    Added routing and lazy loading

--- Client/src/app/shop/shop.service.ts
+++ Client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IProductBrand } from '../shared/models/productBrand';
 import { IProductType } from '../shared/models/productType';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -34,5 +35,9 @@ export class ShopService {
 
   getTypes() {
     return this.http.get<IProductType[]>(this.baseUrl + 'products/types');
+  }
+
+  getProduct(id: string) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 }
