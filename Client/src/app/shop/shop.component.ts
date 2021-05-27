49558861d00e54b7082091a30838c223f3bd9efe
Mikhail Kiyan <Kiyan@ukr.net>
2021-05-28 01:30:28 +0300

    Added loading spinner and a home component

--- Client/src/app/shop/shop.component.ts
+++ Client/src/app/shop/shop.component.ts
@@ -11,7 +11,7 @@ import { ShopService } from './shop.service';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit, OnDestroy {
-  @ViewChild('search', {static: true}) searchTerm: ElementRef | null = null;
+  @ViewChild('search', { static: false } ) searchTerm: ElementRef | null = null;
   products: IProduct[] = [];
   brands: IProductBrand[] = [];
   types: IProductType[] = [];
